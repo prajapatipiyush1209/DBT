14ba1c2d0b16f991141f72c03e8a4f42af7f02b3
Piyush Tech <prajapatipiyush1209@gmail.com>
2026-09-06 19:17:55 +0530

    Added after created silver

--- dbt_learning_databricks/models/Bronze/bronze.sql
+++ dbt_learning_databricks/models/Bronze/bronze.sql
@@ -1,23 +1,3 @@
---{{ config(materialized='view') }}
--- Block layer configuration for the bronze model highest priority of the configuration is given to the block layer configuration. This means that if there are any conflicting configurations between the block layer and the properties.yml file, the block layer configuration will take precedence. #}
-
---  By using the below query generic test on the transaction_id column is performed to check for uniqueness and not null values. 
---  The test is failing due to the presence of duplicate and null values in the transaction_id column. 
-    {# SELECT *
-FROM {{ source('source', 'sales') }} #}
-
--- Removing the duplicate and null values from the transaction_id column to make the test pass.
-    WITH ranked AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY `Transaction Id`
-            ORDER BY `Transaction Id`
-        ) AS rn
+ SELECT
+        *
     FROM {{ source('source', 'sales') }}
-    where `Transaction Id` is not null
-)
-
-SELECT *
-FROM ranked
-WHERE rn = 1
---
+++ dbt_learning_databricks/models/Silver/fact_sales.sql
@@ -0,0 +1,63 @@
+--{{ config(materialized='view') }}
+-- Block layer configuration for the bronze model highest priority of the configuration is given to the block layer configuration. This means that if there are any conflicting configurations between the block layer and the properties.yml file, the block layer configuration will take precedence. #}
+
+--  By using the below query generic test on the transaction_id column is performed to check for uniqueness and not null values. 
+--  The test is failing due to the presence of duplicate and null values in the transaction_id column. 
+    {# SELECT *
+FROM {{ source('source', 'sales') }} #}
+
+-- Removing the duplicate and null values from the transaction_id column to make the test pass.
+
+--============================================================
+    -- Now, I want to create a incremenal load of the data to 
+        -- create the fact_sales table in the silver layer.
+--============================================================
+
+
+--===========================================================================
+    -- Remove duplicates and null values from the transaction_id column to make the test pass.
+--===========================================================================
+{{ config(
+    materialized='incremental',
+    unique_key='Transaction Id',
+    catalog='silver',
+    schema='silver_layer'
+    ) }}
+
+    WITH ranked AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY `Transaction Id`
+            ORDER BY `Transaction Id`
+        ) AS rn
+    FROM {{ ref('bronze') }}
+    where `Transaction Id` is not null
+    {% if is_incremental() %}
+    and `Date` > (select max(`Date`) from {{ this }})
+    {% endif %}
+
+)
+
+--==========================================================================
+    --Final query to create the fact_sales table in the silver layer.
+--==========================================================================
+SELECT
+    CAST(`Transaction Id` AS STRING) AS `transaction_id`,
+    CAST(`Date` AS DATE) AS `date`,
+    CAST(`Product Id` AS STRING) AS `product_id`,
+    CAST(`Quantity` AS INT) AS `quantity`,
+
+    CAST(
+        REPLACE(`PPU`, ',', '')
+        AS DECIMAL(18,2)
+    ) AS `ppu`,
+    CAST(
+        REPLACE(`Amount`, ',', '')
+        AS DECIMAL(18,2)
+    ) AS `amount`,
+    CAST(`Customer Id` AS STRING) AS `customer_id`,
+    CAST(`Region` AS STRING) AS `region`
+
+FROM ranked
+WHERE rn = 1
